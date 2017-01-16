b5da2c385a3e5628d4faf41ad853962574d7637d
JonathanSchwob <jonathanrschwob@gmail.com>
2017-01-16 12:15:19 -0800

    option to post link

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ app.controller('MainController', function($scope) {
   ];
   
   $scope.addPost = () => {
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    if (!$scope.title || $scope.title === '') {
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      upvotes: 0,
+      links: $scope.link});
+
     $scope.title = '';
+    $scope.link = '';
   };
 
   $scope.incrementUpvotes = (post) => {
