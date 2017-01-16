4011d16c3ef12282f74f6722d4a843144373cadf
JonathanSchwob <jonathanrschwob@gmail.com>
2017-01-16 12:02:05 -0800

    add upvote function

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ app.controller('MainController', function($scope) {
   $scope.posts = [
     {title: 'Hack Reactor awards $1.3 million in scholarship funds', upvotes: 5},
     {title: 'Makersquare brand changes to Hack Reactor', upvotes: 6},
-    {title: 'Font ligatures, helpful or not?', upvotes: -3},
+    {title: 'Font ligatures, helpful or annoying?', upvotes: -3},
   ];
   
   $scope.addPost = () => {
     $scope.posts.push({title: $scope.title, upvotes: 0});
     $scope.title = '';
+  };
 
+  $scope.incrementUpvotes = (post) => {
+    post.upvotes++;
   };
 });
