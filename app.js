270cc416698d862a90dd8d2919c71ccf5d7bdfcf
JonathanSchwob <jonathanrschwob@gmail.com>
2017-01-16 13:42:33 -0800

    begin putting static data from front to back

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module('reactorNews', []);
 
-app.controller('MainController', function($scope) {
+app.controller('MainController', ($scope) => {
   $scope.posts = [
     {title: 'Hack Reactor awards $1.3 million in scholarship funds', upvotes: 5},
     {title: 'Makersquare brand changes to Hack Reactor', upvotes: 6},
--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
+mongoose.connect('mongodb://localhost/myapp');
+
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we are connected');
+});
+
+var postSchema = new Schema(
+  {
+    title: String,
+    link: String
+  }
+);
+
+var Post = mongoose.model('Post', postSchema);
 
 app.use(express.static(__dirname));
-
 app.listen(4000, console.log('listening on port 4000'));
